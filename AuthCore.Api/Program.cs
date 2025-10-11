0c2c24a919ff48c12a953605fc6c109a96e5db4f
Eric <ericsilva333@outlook.com>
2025-10-11 20:01:42 -0300

    Ajuste no program.cs

--- AuthCore.Api/Program.cs
+++ AuthCore.Api/Program.cs
@@ -18,6 +18,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerDocumentation();
 }
 
+app.UseSwaggerDocumentation();
+
 app.UseExceptionHandling();
 app.UseHttpsRedirection();
 app.UseAuthentication();
