ef11b5d9f559d1bf84d85a1a7a9b1e81e976f96e
Eric <ericsilva333@outlook.com>
2026-04-03 14:18:04 -0300

    fix: mailCore logs.

--- src/backend/MailCore/MailCore.Worker/Program.cs
+++ src/backend/MailCore/MailCore.Worker/Program.cs
@@ -5,9 +5,6 @@ var builder = Host.CreateApplicationBuilder(args);
 
 builder.Logging.ClearProviders();
 
-if (builder.Environment.IsDevelopment())
-    builder.Logging.AddSimpleConsole();
-
 builder.Services.AddMailCore(builder.Configuration);
 builder.Services.AddHostedService<Worker>();
 
